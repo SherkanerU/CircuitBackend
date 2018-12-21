8d4920d9e66e27dec9bb71f84f31d914f7210ebf
SherkanerU <42780965+SherkanerU@users.noreply.github.com>
2018-12-21 13:45:18 -0500

    first classes

---
+++ ResistorNetwork.java
@@ -0,0 +1,14 @@
+/********************************************************************************
+This class is meant to represent a reistor network as a weighted graph
+it will be undirected in order to easily program the node volatage matehamtics
+since you can go through the postive or negative termimal but the edge class 
+will allow for detection of travel direction
+********************************************************************************/
+
+
+public class ResistorNetwork
+{
+	private static int V;
+	private int 
+
+}
